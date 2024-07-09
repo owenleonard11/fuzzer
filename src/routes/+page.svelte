15d5dc48f59ebaa0ca267fbd3949de4c3742c807
Owen Leonard <owenleonard11@gmail.com>
2024-07-09 18:01:06 -0400

    Update description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <div class="card text-xl p-2 mx-8 mt-4">
 	<p>
 		Fuzzer is a set of free utilities for finding and processing strings that are mostly (but not exactly) the same. 
-		If your data has "Beatles, The" and "The Beatles," this could be the tool for you. Choose an option below or see
+		If your data has columns for "Beatles, The" and "The Beatles," this could be the tool for you. 
+		Choose an option below or see
 		<strong>Usage</strong> to get started.
 	</p> 
+</div>
+
+<div class="card">
+
 </div>
