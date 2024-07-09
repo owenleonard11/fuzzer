17a0c56a60af82b62dd8323c60546fbcbaf96f49
Owen Leonard <owenleonard11@gmail.com>
2024-07-09 14:49:45 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+	import '../app.postcss'
+	import { TabGroup, TabAnchor, Modal } from '@skeletonlabs/skeleton'
+	import { page } from '$app/stores';
+
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+	
+</script>
+
+<Modal />
+
+<TabGroup class="text-3xl px-8 pt-8">
+	<TabAnchor selected = {$page.url.pathname === "/"} href="/">
+		<svelte:fragment slot="lead">Fuzzer</svelte:fragment>
+	</TabAnchor>
+	<TabAnchor selected = {$page.url.pathname === "/usage"} href="/usage">Usage</TabAnchor>
+	<TabAnchor selected = {$page.url.pathname === "/about"} href="/about">About</TabAnchor>
+</TabGroup>
+
+<slot />
+<div class="card text-xl p-4 mx-8">
+	<code class="code">v0.1</code>
+	<span>|</span> 
+	<a href="https://github.com/owenleonard11/titlegrok/" class="anchor">GitHub</a>
+	<span>|</span>
+	<a href="https://titlegrok.readthedocs.io/" class="anchor">ReadTheDocs</a>
+</div>
