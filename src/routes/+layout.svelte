0977cb40ceadadc7354d673e0d82306e473dd69c
Owen Leonard <owenleonard11@gmail.com>
2024-07-09 17:55:02 -0400

    Move shared elements to layout, add description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
-	import '../app.postcss';
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import '../app.postcss'
+	import { TabGroup, TabAnchor, Modal } from '@skeletonlabs/skeleton'
+	import { page } from '$app/stores';
+
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+	
 </script>
 
+<Modal />
+
+<TabGroup class="text-3xl px-8 pt-8">
+	<TabAnchor selected = {$page.url.pathname === "/"} href="/">
+		<svelte:fragment slot="lead">Fuzzer</svelte:fragment>
+	</TabAnchor>
+	<TabAnchor selected = {$page.url.pathname === "/usage"} href="/usage">Usage</TabAnchor>
+	<TabAnchor selected = {$page.url.pathname === "/about"} href="/about">About</TabAnchor>
+</TabGroup>
+
 <slot />
+
+<div class="card text-xl p-2 mx-8 mt-4">
+	<code class="code">v0.1</code>
+	<span class="text-slate-500">|</span> 
+	<a href="https://github.com/owenleonard11/fuzzer" class="anchor">GitHub</a>
+	<span class="text-slate-500">|</span> 
+	<a href="https://fuzzer.readthedocs.io/en/latest/" class="anchor">ReadTheDocs</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,7 @@
-<script lang="ts">
-
-	import '../app.postcss'
-	import { TabGroup, TabAnchor, Modal } from '@skeletonlabs/skeleton'
-	import { page } from '$app/stores';
-
-	import { initializeStores } from '@skeletonlabs/skeleton';
-	initializeStores();
-	
-</script>
-
-<Modal />
-
-<TabGroup class="text-3xl px-8 pt-8">
-	<TabAnchor selected = {$page.url.pathname === "/"} href="/">
-		<svelte:fragment slot="lead">Fuzzer</svelte:fragment>
-	</TabAnchor>
-	<TabAnchor selected = {$page.url.pathname === "/usage"} href="/usage">Usage</TabAnchor>
-	<TabAnchor selected = {$page.url.pathname === "/about"} href="/about">About</TabAnchor>
-</TabGroup>
-
-<slot />
-<div class="card text-xl p-4 mx-8">
-	<code class="code">v0.1</code>
-	<span>|</span> 
-	<a href="https://github.com/owenleonard11/fuzzer" class="anchor">GitHub</a>
-	<span>|</span>
-	<a href="https://fuzzer.readthedocs.io/en/latest/" class="anchor">ReadTheDocs</a>
+<div class="card text-xl p-2 mx-8 mt-4">
+	<p>
+		Fuzzer is a set of free utilities for finding and processing strings that are mostly (but not exactly) the same. 
+		If your data has "Beatles, The" and "The Beatles," this could be the tool for you. Choose an option below or see
+		<strong>Usage</strong> to get started.
+	</p> 
 </div>
